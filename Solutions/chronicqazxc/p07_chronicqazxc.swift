107b1e4e8011e4790f6e866a60d830a7cbb4fd01
Wayne Hsiao <chronicqazxc@gmail.com>
2018-08-12 11:17:52 +0800

    add p07 🎉

---
+++ Solutions/chronicqazxc/p07_chronicqazxc.swift
@@ -0,0 +1,37 @@
+//
+//  p07_chronicqazxc.swift
+//  chronicqazxc
+//
+//  Created by Wayne Hsiao on 2018/8/7.
+//
+
+import Foundation
+import NinetyNineSwiftProblems
+
+extension List {
+
+    func flatten() -> List {
+        
+        var flatMap = [T]()
+        
+        func recursive(list:List) {
+
+            for index in 0...list.length-1 {
+
+                guard let item = list[index] else {
+                    return
+                }
+
+                if let nextList = item as? List {
+                    recursive(list: nextList)
+                } else {
+                    flatMap.append(item)
+                }
+            }
+        }
+        
+        recursive(list: self)
+        
+        return List(flatMap)
+    }
+}
