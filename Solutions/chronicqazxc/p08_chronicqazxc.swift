392902762b6e09d660557f8aeedf83184d61c866
Wayne Hsiao <chronicqazxc@gmail.com>
2018-08-14 21:06:23 +0800

    add p08 🎉

---
+++ Solutions/chronicqazxc/p08_chronicqazxc.swift
@@ -0,0 +1,28 @@
+//
+//  p08_chronicqazxc.swift
+//  chronicqazxc
+//
+//  Created by Wayne Hsiao on 2018/8/12.
+//
+
+import Foundation
+import NinetyNineSwiftProblems
+
+extension List where T: Equatable {
+    
+    func compress() -> List {
+        var result = [T]()
+        var currentItem = self
+        result.append(currentItem.value)
+        while let nextItem = currentItem.nextItem {
+            if currentItem.value == nextItem.value {
+                currentItem = nextItem
+            } else {
+                currentItem = nextItem
+                result.append(currentItem.value)
+            }
+        }
+        return List(result)
+    }
+}
+
